9ad75d198130a56452054f8750e22523441bd845
githubdlp <maildlp@gmail.com>
2017-10-25 13:09:58 +0530

    Update tests.py

--- tests.py
+++ tests.py
@@ -7,7 +7,7 @@ class FlaskrTestCase(unittest.TestCase):
         
     def test_index(self):
         rv = self.app.get('/')
-        assert b'hello, 1world' in rv.data
+        assert b'hello, 2world' in rv.data
         
         
 if __name__ == '__main__':
